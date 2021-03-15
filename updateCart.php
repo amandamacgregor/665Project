04334ebeeb87059feb0479f37d8c5933b08d4516
amandamacgregor <amanda.macgregor13@gmail.com>
2021-03-15 13:41:17 -0600

    update cart added

---
+++ updateCart.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+/*
+    Purpose: Update Cart (no display)
+    Author: AM & TS
+    Date: March 2021
+    Uses: ShoppingCart
+ */
+
+// automatically load required Class files
+
+spl_autoload_register(function ($class_name){
+    include $class_name . '.php';
+});
+
+session_start();
+
+// if the form element productid is set
+
+if (isset($_POST['productid']))
+{
+    // if the session element aCart is not set
+
+    if (!isset($_SESSION['aCart']))
+    {
+        // instantiate a d9RWSShopCart object and store it in $_SESSION
+
+        $_SESSION['aCart'] = new HappyEarthCart();
+    }
+    
+    // if the form element productqty is set (if the user updates the quantity for a product in their shopping cart)
+
+    if (isset($_POST['productqty']))
+    {
+        // call the updateCartItem method
+
+       $_SESSION['aCart']->updateCartItem((int)$_POST['productid'],(int)$_POST['productqty']);
+    }
+    else
+    {
+        // call the addCartItem method
+        
+        $_SESSION['aCart']->addCartItem((int)$_POST['productid']);
+    }
+}
+header('location:viewCart.php');
+exit();
+?>
